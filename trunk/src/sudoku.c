a2c7d983e0985920de31c1b206fdc3bd94c5bfc0
binogure <binogure@2f5606fa-d4ed-11dd-8179-53c65a4d6d95>
2009-01-07 18:18:33 +0000

    Sudoku.c et parseur integre

---
+++ trunk/src/sudoku.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdint.h>
+#include <ctype.h>
+
+int nb_color = 0;
+int grid_size = 10;
+uint16_t *grid;
+
+
+void
+fill_grid(uint16_t color, uint16_t* tmp_grid, int position)
+{
+  if(isxdigit(color))
+    {
+      tmp_grid[position]=color;
+    }
+  else
+    {
+      if(color=='_')
+	{
+	  *(tmp_grid+position)= -1;
+	}
+    }
+}
+
+void
+print_grid(uint16_t *grid)
+{
+  int loop = nb_color*nb_color;
+  int i;
+  for(i=0; i<=loop;++i)
+    {
+      printf("%c ",grid[i]);
+      if((i+1)%nb_color==0 && i!=0)
+	printf("\n");
+    }
+}
+
+void
+grid_cpy(uint16_t* tmp_grid)
+{
+  int i;
+  int loop = nb_color*nb_color;
+  for(i=0;i<=loop;++i)
+    {
+      grid[i]=tmp_grid[i];
+    }
+}
+
+void
+parsing(char *file)
+{
+  FILE *my_file = NULL;
+  int buf_tmp = 0;
+  int position = 0;
+  uint16_t* tmp_grid=malloc(grid_size * sizeof(uint16_t));
+  my_file = fopen(file,"r");
+  if(my_file == NULL)
+    {
+      perror(file);
+      exit(0);
+    }
+  buf_tmp = fgetc(my_file);
+  while (buf_tmp != EOF)
+    {
+      if(grid_size<nb_color)
+	{
+	  grid_size = grid_size*2;
+	  tmp_grid = realloc(tmp_grid, grid_size * sizeof(uint16_t)); 
+	}
+      nb_color++;
+      fill_grid((uint16_t)buf_tmp,tmp_grid,position);
+      if(!isspace(buf_tmp))
+	 position++;
+      buf_tmp = fgetc(my_file);
+    }
+  
+  fclose(my_file);
+  grid = malloc(nb_color+1 * sizeof(uint16_t));
+  nb_color = sqrt(nb_color/2);
+  grid_cpy(tmp_grid);
+}
+
+int 
+main(int argc, char **argv) 
+{ 
+  parsing(argv[1]);
+
+  print_grid(grid);
+
+  printf("nombre de couleurs : %d\n",nb_color);
+
+  return EXIT_SUCCESS;
+}
