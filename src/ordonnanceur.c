3213118d3214a0ff410769b83863b45fd2ff3e26
Xavier Sellier <xsellier@gmail.com>
2017-10-05 14:21:19 -0400

    Lint: lint the code fixes compilation warning, add an example

--- src/ordonnanceur.c
+++ src/ordonnanceur.c
@@ -72,21 +72,19 @@ pthread_t PopFront(stack *l)
    return val;
 }
 
-uint16_t
-search_next_position(uint16_t position, uint16_t* grid)
-{
-  uint16_t i=position;
-  for(;i<=nb_color*nb_color;++i)
-    {
-      if(grid[i]==0xFF)
-	return i; 
+uint16_t search_next_position(uint16_t position, uint16_t* grid) {
+  uint16_t index = position;
+
+  for(;index <= nb_color * nb_color; ++index) {
+    if (grid[index] == 0xFF) {
+      return index;
     }
-  return i;
+  }
+
+  return index;
 }
  
-bool
-grid_check(uint16_t* grid)
-{
+bool grid_check(uint16_t* grid) {
   uint16_t d = sqrt(nb_color);
   uint16_t position = 0;
   uint16_t color;
@@ -94,71 +92,84 @@ grid_check(uint16_t* grid)
   uint16_t tmp;
 
   /* reach every position */
-  for(position = 0; position < (nb_color-1) * (nb_color); ++position)
-    {
-      while(grid[position]==0xFF && position<(nb_color-1) * nb_color )
-	{
-	  position++;
-	}
-      color = grid[position];
-      if(position>=nb_color*nb_color)
-	return 1;
-      /* checking columns */
-      tmp_pos = position % nb_color;
-      for(; tmp_pos < nb_color * nb_color; tmp_pos = tmp_pos+nb_color)
-	{
-	  if(grid[tmp_pos] == color && grid[tmp_pos]!= 0xFF && tmp_pos != position)
-	    return false;
-	}
-      
-      /* checking line */
-      tmp_pos = position;
-      while(tmp_pos%nb_color!=0)
-	tmp_pos--;
-      tmp = tmp_pos;
-      for(; tmp_pos < tmp + nb_color; ++tmp_pos)
-	{
-	  if(grid[tmp_pos] == color && grid[tmp_pos]!= 0xFF && tmp_pos != position)
-	    return false;
-	}
-
-      /* checking blocks */
-      tmp_pos = position;
-      while(tmp_pos%d!=0)
-	tmp_pos--;
-      do 
-	{
-	  if(grid[tmp_pos] == color && grid[tmp_pos]!= 0xFF && tmp_pos != position)
-	    return false;
-	  ++tmp_pos;
-	  if(tmp_pos % d == 0)
-	    tmp_pos = tmp_pos + nb_color - d;
-	} while (tmp_pos < d * nb_color);
+  for (position = 0; position < ((nb_color-1) * nb_color); ++position) {
+    while (grid[position]==0xFF && position < ((nb_color-1) * nb_color)) {
+      position++;
     }
+
+    color = grid[position];
+
+    if (position >= (nb_color * nb_color)) {
+      return 1;
+    }
+
+    /* checking columns */
+    tmp_pos = position % nb_color;
+
+    for (; tmp_pos < nb_color * nb_color; tmp_pos = tmp_pos + nb_color) {
+	    if (grid[tmp_pos] == color && grid[tmp_pos]!= 0xFF && tmp_pos != position) {
+        return false;
+      }
+	  }
+      
+    /* checking line */
+    tmp_pos = position;
+    while ((tmp_pos % nb_color) != 0) {
+      tmp_pos--;
+    }
+
+    tmp = tmp_pos;
+    for(; tmp_pos < (tmp + nb_color); ++tmp_pos) {
+      if(grid[tmp_pos] == color && grid[tmp_pos]!= 0xFF && tmp_pos != position) {
+        return false;
+      }
+	  }
+
+    /* checking blocks */
+    tmp_pos = position;
+
+    while(tmp_pos%d!=0) {
+      tmp_pos--;
+    }
+
+    do {
+	    if (grid[tmp_pos] == color && grid[tmp_pos]!= 0xFF && tmp_pos != position) {
+        return false;
+      }
+
+      ++tmp_pos;
+
+	    if ((tmp_pos % d) == 0) {
+        tmp_pos = tmp_pos + nb_color - d;
+      }
+	  } while (tmp_pos < (d * nb_color));
+  }
+
   return 1;
 }
 
 void
 ordonnanceur(uint16_t* grid)
 {
-  int i;
+  int index;
   pthread_t* thread = malloc(nb_color*sizeof(pthread_t));
   pthread_t tmp;
   pthread_attr_t new_attr;
   pthread_mutex_t verrou;
  
-  if(grid_check(grid)){
-  pthread_attr_init(&new_attr);
-  pthread_attr_setschedpolicy(&new_attr,SCHED_RR);
+  if (grid_check(grid)) {
+    pthread_attr_init(&new_attr);
+    pthread_attr_setschedpolicy(&new_attr,SCHED_RR);
 
-  Init(&My_stack);
+    Init(&My_stack);
 
-  pthread_mutex_init(&verrou,NULL);
-  
-  for(i=0;i<nb_color;++i)
-      PushBack(&My_stack,thread[i]);
-  while(is_change!=0)
-    {
+    pthread_mutex_init(&verrou,NULL);
+    
+    for (index = 0; index < nb_color; ++index) {
+      PushBack(&My_stack,thread[index]);
+    }
+
+    while (is_change!=0) {
       tmp=PopFront(&My_stack);
       pthread_mutex_lock (&verrou);      
       pthread_create (&tmp, &new_attr,max_solver, grid);
@@ -169,90 +180,97 @@ ordonnanceur(uint16_t* grid)
   }
 }
 
-void* max_solver(void* test)
-{
+void* max_solver(void* test) {
   uint16_t d = sqrt(nb_color);
   uint16_t position = 0;
   uint16_t tmp_pos;
   uint16_t tmp;
   int nb_available = 0;
-  uint16_t i;
+  uint16_t index;
   bool* c_available = malloc(nb_color * sizeof(bool));
   uint16_t* grid = (uint16_t *)test;
   
   is_change=0;
   
-  for(position = 0; position < (nb_color-1) * (nb_color); ++position)
-    {
-
-      for(i=0x00;i<nb_color;++i)
-        c_available[i]=0x01;
+  for (position = 0; position < (nb_color-1) * (nb_color); ++position) {
+      for (index = 0x00; index < nb_color; ++index) {
+        c_available[index] = 0x01;
+      }
      
-      while(grid[position]!=0xFF && (nb_color-1) * nb_color )
-          position++;
+      while (grid[position] != 0xFF && position < ((nb_color-1) * nb_color)) {
+        position++;
+      }
 
       /* filling columns */
       tmp_pos = position % nb_color;
-      for(; tmp_pos < nb_color * nb_color; tmp_pos = tmp_pos+nb_color)
-        {
-         if(grid[tmp_pos]!= 0xFF){
-	    if(grid[tmp_pos]> 57)
-	      c_available[(grid[tmp_pos]-65)]=0;
-	    else
-              c_available[(grid[tmp_pos]-48)]=0;
-	  }
+
+      for (; tmp_pos < nb_color * nb_color; tmp_pos = tmp_pos + nb_color) {
+        if (grid[tmp_pos] != 0xFF) {
+	        if(grid[tmp_pos]> 57) {
+            c_available[(grid[tmp_pos]-65)]=0;
+          } else {
+            c_available[(grid[tmp_pos]-48)]=0;
+          }
         }
+      }
      
       /* filling line */
       tmp_pos = position;
-      while(tmp_pos%nb_color!=0)
+      while(tmp_pos%nb_color!=0) {
         tmp_pos--;
+      }
+
       tmp = tmp_pos;
-      for(; tmp_pos < tmp + nb_color; ++tmp_pos)
-        {
-          if(grid[tmp_pos]!= 0xFF){
-	    if(grid[tmp_pos]> 57)
-	      c_available[(grid[tmp_pos]-65)]=0;
-	    else
-              c_available[(grid[tmp_pos]-48)]=0;
-	  }
+
+      for (; tmp_pos < tmp + nb_color; ++tmp_pos) {
+        if (grid[tmp_pos]!= 0xFF) {
+	        if (grid[tmp_pos]> 57) {
+            c_available[(grid[tmp_pos]-65)]=0;
+          } else {
+            c_available[(grid[tmp_pos]-48)]=0;
+          }
         }
+      }
      
       /* filling blocks */
       tmp_pos = position;
-      while(tmp_pos%d!=0)
+      while (tmp_pos % d != 0) {
         tmp_pos--;
-      do
-        {
-          if(grid[tmp_pos]!= 0xFF){
-	    if(grid[tmp_pos]> 57)
-	      c_available[(grid[tmp_pos]-65)]=0;
-	    else
-              c_available[(grid[tmp_pos]-48)]=0;
-	  }
-          ++tmp_pos;
-          if(tmp_pos % d == 0)
-            tmp_pos = tmp_pos + nb_color - d;
-        } while (tmp_pos < d * nb_color);
+      }
+
+      do {
+        if (grid[tmp_pos]!= 0xFF) {
+	        if(grid[tmp_pos]> 57) {
+	          c_available[(grid[tmp_pos]-65)]=0;
+          } else {
+            c_available[(grid[tmp_pos]-48)]=0;
+          }
+        }
+        ++tmp_pos;
+
+        if(tmp_pos % d == 0) {
+          tmp_pos = tmp_pos + nb_color - d;
+        }
+      } while (tmp_pos < (d * nb_color));
      
       /* filling grid */
       nb_available = 0;
-      for(i=0x00;i<nb_color;++i)
-        {
-          if(c_available[i]==1)
-            {
-              tmp = i;
-              nb_available++;
-            }
+      for (index = 0x00; index < nb_color; ++index) {
+        if (c_available[index] == 1) {
+          tmp = index;
+          nb_available++;
         }
-      if(nb_available==1)
-        {
-          is_change=1;
-	    if(tmp+48>57)
-	      grid[position]=tmp+65;
-	    else
-	      grid[position]=tmp+48;
+      }
+
+      if (nb_available == 1) {
+        is_change = 1;
+
+	      if (tmp + 48 > 57) {
+	        grid[position] = tmp + 65;
+        } else {
+	        grid[position] = tmp + 48;
         }
+      }
     }
   return test;
 }
--- src/sudoku.c
+++ src/sudoku.c
@@ -1,196 +1,224 @@
 #include "sudoku.h"
 
-void
-fill_grid(uint16_t color, uint16_t* tmp_grid, int position)
-{
-  if(isxdigit(color))
-    {
-      tmp_grid[position]=color;
-    }
-  else
-    {
-      if(color=='_')
-	{
-	  *(tmp_grid+position)= 0xFF;
-	}
-    }
+void fill_grid(uint16_t color, uint16_t* tmp_grid, int position) {
+  if (isxdigit(color)) {
+      tmp_grid[position] = color;
+  } else {
+    if (color == '_') {
+	    *(tmp_grid + position) = 0xFF;
+	  }
+  }
 }
 
-void
-print_grid(uint16_t *grid)
-{
+void print_grid(uint16_t *grid) {
   int loop = nb_color*nb_color;
-  int i;
-  for(i=0; i<=loop;++i)
-    {
-      printf("%c ",grid[i]);
-      if((i+1)%nb_color==0 && i!=0)
-	printf("\n");
+  int index;
+
+  for (index = 0; index <= loop; ++index) {
+    printf("%c ",grid[index]);
+
+    if((index+1)%nb_color==0 && index!=0) {
+      printf("\n");
     }
+  }
+
   printf("\n");
 }
 
 void
 grid_cpy(uint16_t* tmp_grid)
 {
-  int i;
+  int index;
   int loop = nb_color*nb_color;
-  for(i=0;i<=loop;++i)
+  for(index=0;index<=loop;++index)
     {
-      grid2[i]=tmp_grid[i];
+      grid2[index]=tmp_grid[index];
     }
 }
 
 void
 grid_cpy_invert(uint16_t* tmp_grid, uint16_t* grid)
 {
-  int i;
-  int loop = nb_color*nb_color;
-  for(i=0;i<=loop;++i)
-    {
-      tmp_grid[i]=grid[i];
-    }
+  int index;
+  int loop = nb_color * nb_color;
+
+  for ( index = 0; index <= loop; ++index) {
+    tmp_grid[index]=grid[index];
+  }
 }
 
-void
-parsing(char *file)
-{
+void parsing(char *file) {
   FILE *my_file = NULL;
   int buf_tmp = 0;
   int position = 0;
   uint16_t* tmp_grid=malloc(grid_size * sizeof(uint16_t));
   my_file = fopen(file,"r");
-  if(my_file == NULL)
-    {
-      perror(file);
-      exit(0);
-    }
+
+  if(my_file == NULL) {
+    perror(file);
+    exit(0);
+  }
+
   buf_tmp = fgetc(my_file);
-  while (buf_tmp != EOF)
-    {
-      if(grid_size<nb_color)
-	{
-	  grid_size = grid_size*2;
-	  tmp_grid = realloc(tmp_grid, grid_size * sizeof(uint16_t)); 
-	}
-      nb_color++;
-      fill_grid((uint16_t)buf_tmp,tmp_grid,position);
-      if(!isspace(buf_tmp))
-	 position++;
-      buf_tmp = fgetc(my_file);
+
+  // Keep reading the file until the file has been read to the end
+  while (buf_tmp != EOF) {
+
+    if (grid_size < nb_color) {
+      grid_size = grid_size * 2;
+      tmp_grid = realloc(tmp_grid, grid_size * sizeof(uint16_t)); 
     }
+
+    nb_color++;
+
+    fill_grid((uint16_t)buf_tmp, tmp_grid, position);
+
+    if (!isspace(buf_tmp)) {
+      position++;
+    }
+
+    buf_tmp = fgetc(my_file);
+  }
   
   fclose(my_file);
-  grid2 = malloc(nb_color+1 * sizeof(uint16_t));
+
+  grid2 = malloc(nb_color + 1 * sizeof(uint16_t));
   nb_color = sqrt(nb_color/2);
+
   grid_cpy(tmp_grid);
 }
  
 int
 main(int argc, char **argv) 
 { 
-  uint16_t* grid=NULL;
+  if (argc != 2) {
+    printf("It takes one argument, the grid file only, no more no less\n");
+
+    return EXIT_FAILURE;
+  }
+
+  uint16_t* grid = NULL;
+
   parsing(argv[1]);
-  grid = malloc(nb_color*nb_color * sizeof(uint16_t));
+
+  printf("Detected size of the grid: %i\n", nb_color);
+
+  grid = malloc(nb_color * nb_color * sizeof(uint16_t));
+
   grid_cpy_invert(grid,grid2);
+
   sudoku(grid);
+
   return EXIT_SUCCESS;
 }
 
-void
-search_color(bool* c_available, uint16_t position, uint16_t* grid)
-{
+void search_color(bool* c_available, uint16_t position, uint16_t* grid) {
   uint16_t d = sqrt(nb_color);
   uint16_t tmp_pos;
   uint16_t tmp;
-  uint16_t i;
+  uint16_t index;
   
-  for(i=0;i<nb_color;++i)
-    c_available[i]=1;
+  for (index = 0; index < nb_color; ++index) {
+    c_available[index] = 1;
+  }
   
    /* filling columns */
   tmp_pos = position % nb_color;
-  for(; tmp_pos < nb_color * nb_color; tmp_pos = tmp_pos+nb_color)
-        {
-          if(grid[tmp_pos]!= 0xFF){
-	    if(grid[tmp_pos]>57)
-	      c_available[(grid[tmp_pos]-65)]=0;
-	     else
-	      c_available[(grid[tmp_pos]-48)]=0;
-	    
+
+  for (; tmp_pos < nb_color * nb_color; tmp_pos += nb_color) {
+    if (grid[tmp_pos] != 0xFF) {
+	    if (grid[tmp_pos] > 57) {
+        c_available[(grid[tmp_pos] - 65)] = 0;
+      } else {
+	      c_available[(grid[tmp_pos] - 48)] = 0;
+      }
 	  }
 	}
   
   /* filling line */
   tmp_pos = position;
-  while(tmp_pos%nb_color!=0)
+
+  while (tmp_pos % nb_color != 0) {
     tmp_pos--;
+  }
+
   tmp = tmp_pos;
-  for(; tmp_pos < tmp + nb_color; ++tmp_pos)
-    {
-      if(grid[tmp_pos]!= 0xFF){
-	if(grid[tmp_pos]>57){
-	  c_available[(grid[tmp_pos]-65)]=0;
-	} else{
-	  c_available[(grid[tmp_pos]-48)]=0;
-	}
-      }
+
+  for (; tmp_pos < tmp + nb_color; ++tmp_pos) {
+    if (grid[tmp_pos] != 0xFF) {
+	    if (grid[tmp_pos] > 57) {
+	      c_available[(grid[tmp_pos] - 65)] = 0;
+      } else {
+    	  c_available[(grid[tmp_pos] - 48)] = 0;
+	    }
     }
+  }
   
   /* filling blocks */
   tmp_pos = position;
-  while(tmp_pos%d!=0)
+
+  while (tmp_pos % d != 0) {
     tmp_pos--;
-  do
-    {
-      if(grid[tmp_pos]!= 0xFF){
-	if(grid[tmp_pos]>57){
-	  c_available[(grid[tmp_pos]-65)]=0;
-	} else{
-	  c_available[(grid[tmp_pos]-48)]=0;
-	}
-      }
-      ++tmp_pos;
-      if(tmp_pos % d == 0)
-	tmp_pos = tmp_pos + nb_color - d;
-    } while (tmp_pos < d * nb_color); 
+  }
+
+  do {
+    if (grid[tmp_pos] != 0xFF) {
+	    if (grid[tmp_pos] > 57) {
+	      c_available[(grid[tmp_pos] - 65)] = 0;
+	    } else {
+	      c_available[(grid[tmp_pos] - 48)] = 0;
+	    }
+    }
+    ++tmp_pos;
+
+    if (tmp_pos % d == 0) {
+      tmp_pos = tmp_pos + nb_color - d;
+    }
+  } while (tmp_pos < d * nb_color); 
 }
 
-void
-sudoku(uint16_t* grid_old)
-{
-  int i;
+void sudoku(uint16_t* grid_old) {
+  int index;
   uint16_t pos;
-  bool* c=malloc(nb_color*sizeof(bool));
-  bool complete=true;
+  bool* condition = malloc(nb_color*sizeof(bool));
+  bool complete = true;
   uint16_t* grid = malloc(nb_color*nb_color*sizeof(uint16_t));
+
   grid_cpy_invert(grid,grid_old);
-       print_grid(grid);
-  if(grid_check(grid)){ 
+  print_grid(grid);
+
+  if (grid_check(grid)) {
     ordonnanceur(grid);
-    for(i=0;i<nb_color*nb_color;++i)
-      {
-	if(grid[i]==0xFF)
-	  complete=false;
+    
+    for (index = 0; index < (nb_color * nb_color); ++index) {
+	    if(grid[index]==0xFF) {
+        complete=false;
       }
-    if(complete)
+    }
+
+    if (complete) {
       print_grid(grid);
-    else 
-      {
-	pos=0;
-	while(grid[pos]!=0xFF && pos<nb_color*nb_color-1)
-	  pos++;
-	search_color(c,pos,grid);
-	for(i=0;i<nb_color;++i){
-	  if(c[i]){
-	    if(i+48>57)
-	      grid[pos]=i+65;
-	    else
-	      grid[pos]=i+48;
-	    sudoku(grid);
-	  }
-	}
+    } else {
+      pos = 0;
+  
+	    while (grid[pos] != 0xFF && pos < (nb_color * nb_color - 1)) {
+	      pos++;
       }
+
+      search_color(condition, pos, grid);
+
+      for (index = 0; index < nb_color; ++index) {
+        if (condition[index]) {
+          if (index + 48 > 57) {
+            grid[pos] = index + 65;
+          } else {
+            grid[pos] = index + 48;
+          }
+
+          sudoku(grid);
+        }
+      }
+    }
   }
 }
 
